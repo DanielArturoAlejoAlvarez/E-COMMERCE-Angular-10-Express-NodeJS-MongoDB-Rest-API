c7a6b2c2d0173dfd957acc41294c90bf0b2e6f45
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-30 10:50:09 -0500

    Configure frontend routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
-  {path: '/', component: LoginComponent},
+  {path: '', component: LoginComponent},
+  {path: 'user', component: UserComponent},
 ];
 
 @NgModule({
--- client/src/app/components/auth/login/login.component.ts
+++ client/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,9 +9,29 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private _as: AuthService) { }
+  loginForm = {
+    username: '',
+    password: ''
+  }
+
+  constructor(private _as: AuthService, private _router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this._as.signIn(this.loginForm)
+      .subscribe(data=>{
+        if (data.ok) {
+          localStorage.setItem("token", data.token)
+          this._router.navigate(['user'])
+        }else {
+          alert(data.msg)
+        }       
+      },
+      err=>{
+        console.log(err.error.msg)
+      })
   }
 
 }
