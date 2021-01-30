06e701b74e19e1919ee4da8d1276dfaa05cc350f
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-30 09:51:55 -0500

    Creating auth service

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import User from '../models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  API_URI = 'http://127.0.0.1:3000/api/auth'
+
+  signIn(user): Observable<any> {
+    return this._http.post(`${this.API_URI}/login`, user)
+  }
+
 }
