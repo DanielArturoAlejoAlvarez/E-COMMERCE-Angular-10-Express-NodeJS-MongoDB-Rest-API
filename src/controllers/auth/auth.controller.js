0a895b02095dc41ebb9342113c79c4353514057e
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-01-29 19:19:03 -0500

    Creating method login to get tokens

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -7,7 +7,38 @@ const jwt = require("jsonwebtoken");
 const CTRLS = {};
 
 CTRLS.login = (req, res) => {
-  
+  User.findOne({ username: req.body.username }, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err,
+      });
+    }
+
+    if (!user) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Email or Password invalid!",
+      });
+    }
+
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Email or Password invalid!",
+      });
+    }
+
+    let token = jwt.sign({
+        data: user,
+    }, config.secretKey, { expiresIn: "1h" });
+
+    res.status(201).json({
+      ok: true,
+      user,
+      token
+    })
+  });
 };
 
 module.exports = CTRLS;
