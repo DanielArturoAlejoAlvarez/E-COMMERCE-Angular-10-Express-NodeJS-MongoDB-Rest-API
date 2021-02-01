6d9891baf0a76ef7f15b189775e816bba8238f95
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-02-01 13:48:27 -0500

    Create method to save products in prodct controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -12,6 +12,46 @@ CTRLS.getProduct = (req, res) => {
 };
 
 CTRLS.saveProduct = (req, res) => {
+
+  if (!req.files) {
+    return res.json({msg: "No files where uploaded!"})
+  }
+
+  const image = req.files.name 
+
+  image.mv(`../../uploads/products/${image.name}`, (err)=>{
+    if (err) {
+      return res.status(500).send(err)
+    }
+
+    const product = new Product({
+      category: req.body.category,
+      name: req.body.name,
+      excerpt: req.body.excerpt,
+      description: req.body.description,
+      price: req.body.price,
+      stock: req.body.stock,
+      image: image.name,
+      status: req.body.status
+    })
+
+    console.log(product)
+
+  product.save((err,newProduct)=>{
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err
+      })
+    }
+
+    res.status(201).json({
+      ok: true,
+      product: newProduct
+    })
+
+  })
+  })
   
 };
 
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,11 +4,11 @@ const router = Router()
 const productCTRL = require('../controllers/products.controller')
 const { isAuth } = require('../middlewares/authentication')
 
-router.get('/', isAuth, productCTRL.getProducts)
-router.get('/:id', isAuth, productCTRL.getProduct)
+//router.get('/', isAuth, productCTRL.getProducts)
+//router.get('/:id', isAuth, productCTRL.getProduct)
 router.post('/', isAuth, productCTRL.saveProduct)
-router.put('/:id', isAuth, productCTRL.updateProduct)
-router.delete('/:id/:status', isAuth, productCTRL.deleteProduct)
+//router.put('/:id', isAuth, productCTRL.updateProduct)
+//router.delete('/:id/:status', isAuth, productCTRL.deleteProduct)
 
 
 module.exports = router
