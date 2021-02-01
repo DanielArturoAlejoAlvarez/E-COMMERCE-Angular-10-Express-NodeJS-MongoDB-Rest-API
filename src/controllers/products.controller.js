6cba6d9986a65d454eb20e8bce97de11cb743a84
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-02-01 13:03:19 -0500

    Creating routes products from backend

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,26 @@
+const Product = require("../models/Product");
+const bcrypt = require("bcrypt");
+
+const CTRLS = {};
+
+CTRLS.getProducts = (req, res) => {
+ 
+};
+
+CTRLS.getProduct = (req, res) => {
+  
+};
+
+CTRLS.saveProduct = (req, res) => {
+  
+};
+
+CTRLS.updateProduct = (req, res) => {
+  
+};
+
+CTRLS.deleteProduct = (req, res) => {
+  
+};
+
+module.exports = CTRLS;
