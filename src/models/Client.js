31100ec3a6a169849bed00cac4e9994169409674
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-02-02 14:11:07 -0500

    Create client model from backend

---
+++ src/models/Client.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const ClientSchema = new Schema({
+  displayName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+},
+{
+  timestamps: true
+})
+
+module.exports = mongoose.model('Category', ClientSchema)
