da81a8be6c914d2f8366ea8732d5604188d6ec9b
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-02-02 12:17:18 -0500

    Create method saveProduct from product service in frontend with angular we will also use formData

--- client/src/app/services/product.service.ts
+++ client/src/app/services/product.service.ts
@@ -17,7 +17,19 @@ export class ProductService {
     return this._http.get(`${this.API_URI}/products`)
   }
 
-  
+  saveProduct(infoProd, imgProd: File): Observable<any> {
+    const fd = new FormData();
+    fd.append('category', infoProd.category);
+    fd.append('name', infoProd.name);
+    fd.append('excerpt', infoProd.excerpt);
+    fd.append('description', infoProd.description);
+    fd.append('price', infoProd.price);
+    fd.append('stock', infoProd.stock);
+    fd.append('image', imgProd, imgProd.name);
+    fd.append('status', infoProd.status);
+
+    return this._http.post<Product>(`${this.API_URI}/products`, fd);
+  }
 
   
 }
